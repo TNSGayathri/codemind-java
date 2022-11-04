63a69528e8fbb54d5a9710e220eb134c33de4deb
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-11-04 06:01:22 +0530

    import java.util.Scanner; class Space {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s;         int c=0,i;         s=sc.nextLine();         for(i=0;i<s.length();i++)         {             if(s.charAt(i)!=' ')             c+=1;         }         System.out.println(c);     } }

---
+++ without_spaces_number_of_characters.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Space
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s;
+        int c=0,i;
+        s=sc.nextLine();
+        for(i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)!=' ')
+            c+=1;
+        }
+        System.out.println(c);
+    }
+}
