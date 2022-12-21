8fba1012266efaea634cc473f4a447b5738a9270
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-12-21 14:17:45 +0530

    import java.util.*; import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String n;         n=sc.next();     if(n.length()==10 && n.charAt(0)!='0')     {         System.out.println("Valid");     }     else     {         System.out.println("Invalid");     }     } }

---
+++ PHONE_NUMBER_VALIDATION:.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n;
+        n=sc.next();
+    if(n.length()==10 && n.charAt(0)!='0')
+    {
+        System.out.println("Valid");
+    }
+    else
+    {
+        System.out.println("Invalid");
+    }
+    }
+}
